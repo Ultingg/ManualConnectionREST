2a01bfb1f09c2f82051183ee9e43125420de3ac6
Ultingg <utinigg@gmail.com>
2020-09-17 07:42:17 +0300

    Repo refactoring

--- src/main/java/ru/isaykin/services/AuthorsSQLService.java
+++ src/main/java/ru/isaykin/services/AuthorsSQLService.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.String.format;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -33,41 +32,17 @@ public class AuthorsSQLService {
     }
 
 
-    public Author create(Author author) {
-
-        String creationRequest = format("INSERT INTO authors (first_name, last_name, email, birthdate) VALUES " +
-                        "( '%s', '%s', '%s', '%tF')",
-                author.getFirstName(), author.getLastName().replaceAll("'", "\\\\\'"), author.getEmail(), author.getBirthdate());
-        authorsRepositorySQL.requestToTable(creationRequest);
-
-        String feedbackRequest = format("SELECT * FROM authors WHERE first_name = '%s' AND last_name = '%s';",
-                author.getFirstName(), author.getLastName().replaceAll("'", "\\\\\'"));
-        Author author1 = new Author();
-        try {
-            List<Author> authors = authorsRepositorySQL.getListOfAuthors(feedbackRequest);
-            author1 = authors.get(0);
-        } catch (NullPointerException e) {
-            log.error(e.getMessage() + "you get List authors = null. Wrong request to DB");
-
-        }
-        if (author1 != null) return author1;
-
-        return author;
-
-    }
-
     public List<Author> getAll() {
         return authorRepo.getAll();
     }
 
 
     public ResponseEntity<Author> getOneById(Long id) {
-        ResponseEntity<Author> result;
         Author author = authorRepo.getById(id);
         if (author == null) {
-            return result = new ResponseEntity<>(NOT_FOUND);
+            return  new ResponseEntity<>(NOT_FOUND);
         } else {
-            return result = new ResponseEntity<Author>(author, OK);
+            return  new ResponseEntity<>(author, OK);
         }
     }
 
@@ -99,16 +74,13 @@ public class AuthorsSQLService {
     }
 
     public ResponseEntity<Author> delete(Long id) {
-        ResponseEntity<Author> result;
         if (id == null) {
-            return result = new ResponseEntity<>(NOT_FOUND);
+            return  new ResponseEntity<>(NOT_FOUND);
         } else {
             authorRepo.deleteById(id);
-            return result = new ResponseEntity<>(OK);
+            return new ResponseEntity<>(OK);
         }
-        //authorsRepositorySQL.requestToTable(new StringBuilder("DELETE FROM authors WHERE id = ").append(id).toString());
     }
-
 
     public List<Author> getListByAgeGT(int age) {
         Date currentDateMinusYears = Date.
@@ -131,12 +103,12 @@ public class AuthorsSQLService {
 
     public Author insert(Author author) {
 
-         authorRepo.insert(author.getFirstName()
+        authorRepo.insert(author.getFirstName()
                 , author.getLastName()
                 , author.getEmail()
                 , Date.valueOf(author.getBirthdate()));
 
-         return authorRepo.getByEmail(author.getEmail());
+        return authorRepo.getByEmail(author.getEmail());
 
     }
 }
