d2eb8e9d774bd96278594daf554c71d29870d821
ultinigg <Lutik86>
2020-11-26 20:03:48 +0300

    Exception handler and errors added

--- src/main/java/ru/isaykin/exceptions/AuthorExceptionHandler.java
+++ src/main/java/ru/isaykin/exceptions/AuthorExceptionHandler.java
@@ -30,28 +30,6 @@ public class AuthorExceptionHandler extends ResponseEntityExceptionHandler {
 
         return new ResponseEntity<Object>(apiError, HttpStatus.BAD_REQUEST);
     }
-//    @Override
-//    protected ResponseEntity<Object>
-//    handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-//                                 HttpHeaders headers,
-//                                 HttpStatus status, WebRequest request) {
-//
-//        Map<String, Object> body = new LinkedHashMap<>();
-//        body.put("timestamp", new Date());
-//        body.put("status", status.value());
-//
-//        //Get all fields errors
-//        List<String> errors = ex.getBindingResult()
-//                .getFieldErrors()
-//                .stream()
-//                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-//                .collect(Collectors.toList());
-//
-//        body.put("errors", errors);
-//
-//        return new ResponseEntity<>(body, headers, status);
-//
-//    }
     @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
     public ResponseEntity<Object> handleDuplicateException(SQLIntegrityConstraintViolationException exception, WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
